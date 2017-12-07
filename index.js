04aa679b18f27d567e5edcf33cb01a218c2762c5
rochars <rocha.rafaelsilva@gmail.com>
2017-12-07 19:27:35 -0200

    bim!

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-var int8 = new Int8Array(4)
-var int32 = new Int32Array(int8.buffer, 0, 1)
-var float32 = new Float32Array(int8.buffer, 0, 1)
+var float32 = new Float32Array(1);
+var int32 = new Int32Array(float32.buffer);
 
 function pack(i) {
-    int32[0] = i
-    return float32[0]
+    int32[0] = i;
+    return float32[0];
 }
 
 function unpack(f) {
-    float32[0] = f
-    return int32[0]
+    float32[0] = f;
+    return int32[0];
 }
 
-module.exports = pack
-module.exports.pack = pack
-module.exports.unpack = unpack
+module.exports = pack;
+module.exports.pack = pack;
+module.exports.unpack = unpack;
